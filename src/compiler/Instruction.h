d4873d315e1175f9e9f6ffd99f34240da3b92995
SciencePi <sciancepi@gmail.com>
2021-06-21 07:47:48 +0100

    First Commit

---
+++ src/compiler/Instruction.h
@@ -0,0 +1,22 @@
+#ifndef INSTRUCTION_L
+#define INSTRUCTION_L
+
+#define I_ADD  0x00
+#define I_ADC  0x01
+#define I_AND  0x02
+#define I_OR   0x03
+#define I_NOR  0x04
+#define I_CMP  0x05
+#define I_PUSH 0x06
+#define I_POP  0x07
+#define I_LR   0x08
+#define I_SR   0x09
+#define I_MR   0x0A
+#define I_LDH  0x0B
+#define I_LDL  0x0B
+#define I_JNZ  0x0C
+#define I_NOP  0x0D
+#define I_HALT 0x0E
+#define I_SUB  0x0F
+
+#endif
--- src/compiler/Parser.cpp
+++ src/compiler/Parser.cpp
@@ -1,5 +1,7 @@
 #include "Parser.h"
 #include "Lexer.h"
+#include "Instruction.h"
+
 
 #include <vector>
 #include <string>
@@ -10,11 +12,11 @@ std::vector<int> Parser::parseToBytes(std::vector<std::string> tokens, Lexer lex
 
     for (int i = 0; i < tokens.size(); i++){
         if (tokens[i] == "LOAD_ACCUMULATOR_HIGH"){
-            output.push_back(0x1B);
+            output.push_back(I_LDH+0x10);
             output.push_back(stoi(lex.slice(4, tokens[i+1].length(), tokens[i+1])));
         }
         if (tokens[i] == "LOAD_ACCUMULATOR_LOW"){
-            output.push_back(0x0B);
+            output.push_back(I_LDL);
             output.push_back(stoi(lex.slice(4, tokens[i+1].length(), tokens[i+1])));
         }
     }
