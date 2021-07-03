06d97b061c16794c9b0df043dcbe6111d3e06876
SciencePi <sciancepi@gmail.com>
2021-07-03 22:11:52 +0100

    Updated files

--- src/compiler/Parser.cpp
+++ src/compiler/Parser.cpp
@@ -28,6 +28,49 @@ std::vector<int> Parser::parseToBytes(std::vector<std::string> tokens, Lexer lex
                 if (tokens.size()-2 == i) throw 0;
                 // parsing code.
                 if (lex.slice(0, 5, tokens[i+1]) == "IMM8:") {
+                    output.push_back(I_LDH);
+                    output.push_back(stoi(lex.slice(5, tokens[i+1].length(), tokens[i+1])));
+                    i+=2;
+                }
+                else if (lex.slice(0, 3, tokens[i+1]) == "REG"){
+                    output.push_back(I_LDH+0x10);
+                    output.push_back((stoi(lex.slice(tokens[i+1].length()-1, tokens[i+1].length(), tokens[i+1]))));
+                    i+= 2;
+                }
+                else if (tokens[i+1] == "RIGHTBRACKET" && lex.slice(0, 5, tokens[i+2]) == "IMM8:" && tokens[i+3] == "COMMA" &&\
+                 lex.slice(0, 3, tokens[i+4]) == "REG" && tokens[i+5] == "LEFTBRACKET"){
+
+                    output.push_back(I_LDH+0x20);
+                    output.push_back(stoi(lex.slice(5, tokens[i+1].length(), tokens[i+2])));
+                    output.push_back((stoi(lex.slice(tokens[i+4].length()-1, tokens[i+4].length(), tokens[i+4]))));
+                    i += 2;
+                }
+                else if (lex.slice(0, 6, tokens[i+1]) == "IMM16:"){
+                    int integer_value = stoi(lex.slice(6, tokens[i+1].length(), tokens[i+1]));
+                    int upper_8       = (integer_value & 0xff00) >> 8;
+                    int lower_8       = (integer_value & 0x00ff);
+                    output.push_back(I_LDH+0x30);
+
+                    // the system is little endian
+                    output.push_back(lower_8);
+                    output.push_back(upper_8);
+
+                    i += 2;
+                }else{
+                    std::cout << "Error - unexpected token on line: " << current_line << "\n";
+                }
+            }catch(int e){
+                std::cout << "Error on line " << current_line << ".\n";
+            }
+            //output.push_back(I_LDH+0x10);
+            //output.push_back(stoi(lex.slice(4, tokens[i+1].length(), tokens[i+1])));
+        }
+        if (tokens[i] == "LOAD_ACCUMULATOR_LOW"){
+            for (int i = 0; i < tokens.size(); i++)std::cout << tokens[i] << "\n";
+            try{
+                if (tokens.size()-2 == i) throw 0;
+                // parsing code.
+                if (lex.slice(0, 5, tokens[i+1]) == "IMM8:") {
                     output.push_back(I_LDL);
                     output.push_back(stoi(lex.slice(5, tokens[i+1].length(), tokens[i+1])));
                     i+=2;
@@ -64,10 +107,6 @@ std::vector<int> Parser::parseToBytes(std::vector<std::string> tokens, Lexer lex
             }
             //output.push_back(I_LDH+0x10);
             //output.push_back(stoi(lex.slice(4, tokens[i+1].length(), tokens[i+1])));
-        }
-        if (tokens[i] == "LOAD_ACCUMULATOR_LOW"){
-            output.push_back(I_LDL);
-            output.push_back(stoi(lex.slice(5, tokens[i+1].length(), tokens[i+1])));
         }
 
         // preprocessor macros
